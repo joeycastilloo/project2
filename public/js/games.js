1c86c2ac102203f4ebf588b19b11538c13622ff7
FrankReynolds <brooksbaker32@gmail.com>
2021-05-07 12:02:02 -0500

    final js fixes

--- public/js/games.js
+++ public/js/games.js
@@ -1,8 +1,10 @@
-
+const gameInput = document.querySelector('#gamesInput').value
 const gameName = document.querySelector('.gameName')
 const gameBox = document.querySelector('.gameDiv')
 const chBtn = document.querySelector('.choiceBtn')
 const chBtn2 = document.querySelector('.choiceBtn2')
+const rightCol = document.querySelector('.rightColumn')
+const leftCol = document.querySelector('.leftColumn')
 chBtn.style.display = 'none'
 chBtn2.style.display = 'none'
 
@@ -17,12 +19,27 @@ const searchGame = (e) => { console.log('search-game')
         // chBtn.style.display = 'block'
         })
 }
-const addGame = (e) => {
+//Wish list
+const addGameR = (e) => {
+  e.preventDefault();
+  let clickGame = gameName.value
+  let gameTitle = document.createElement('h2')
+  gameTitle.textContent = clickGame
+  rightCol.append(gameTitle)
+  }
   let action = {
-    action: e.target.value, 
+    action: gameInput, 
     name: gameName.value
   }
-
+  //Owned Games
+  const addGameL = (e) => {
+  
+    e.preventDefault();
+    let clickGame = gameName.value
+    let gameTitle = document.createElement('h2')
+    gameTitle.textContent = clickGame
+     leftCol.append(gameTitle)
+    }
 
   $.post('/api/saveGame/', action
   ).then(res=>{ 
@@ -31,9 +48,9 @@ const addGame = (e) => {
     chBtn2.style.display = 'block'
     // chBtn.style.display = 'block'
     })
-}
-chBtn.addEventListener('click', addGame)
-chBtn2.addEventListener('click', addGame)
+
+chBtn.addEventListener('click', addGameL)
+chBtn2.addEventListener('click', addGameR)
 document.querySelector('#searchBtn').addEventListener('click', searchGame)
 
 
